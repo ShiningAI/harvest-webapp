025f821bd48092b70c939470016324f36ef78353
SengMitnick <sengmitnick@163.com>
2024-07-10 18:11:17 +0800

    chore: Update multiple language JSON files for database selection

--- app/[locale]/s/[state]/select/SelectDatabases.tsx
+++ app/[locale]/s/[state]/select/SelectDatabases.tsx
@@ -5,6 +5,7 @@ import { useRequest } from "ahooks";
 import { request } from "@/lib/request";
 import { useRouter } from "next/navigation";
 import { LoaderCircle, CheckCheckIcon } from "lucide-react";
+import { useTranslations } from "next-intl";
 import { useToast } from "@/components/ui/use-toast";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
@@ -29,6 +30,7 @@ interface DatabasesResponse {
 export const SelectDatabases = ({ contactId }: Props) => {
   const { toast } = useToast();
   const { replace } = useRouter();
+  const t = useTranslations("Database");
   const [selectedDatabase, setSelectedDatabase] = useState<string | null>(null);
   const { loading, data, error } = useRequest(async () => {
     const access_token = await getAccessToken(contactId);
@@ -92,51 +94,57 @@ export const SelectDatabases = ({ contactId }: Props) => {
   }
 
   return (
-    <Card>
-      <CardContent className="space-y-4">
-        <div className="grid gap-4 pt-4 box-content">
-          {data.databases.map((database) => (
-            <div
-              key={database.database_id}
-              className={cn(
-                "w-full p-2 flex items-center justify-between rounded-md cursor-pointer overflow-hidden transition-colors",
-                database.database_id === selectedDatabase
-                  ? "bg-primary text-primary-foreground"
-                  : "hover:bg-accent hover:text-accent-foreground"
-              )}
-              onClick={() => setSelectedDatabase(database.database_id)}
-            >
-              <div className="flex-1 overflow-hidden">
-                <h3 className="font-medium">{database.database_title}</h3>
-                <Link
-                  href={database.database_url}
-                  target="_blank"
-                  className="text-sm text-muted-foreground hover:underline"
-                  prefetch={false}
-                >
-                  <span className="truncate block">
-                    {database.database_url}
-                  </span>
-                </Link>
-              </div>
+    <div className="mx-auto max-w-md space-y-6 py-12">
+      <div className="space-y-2 text-center">
+        <h1 className="text-3xl font-bold">{t("MultipleDatabases.title")}</h1>
+        <p className="text-muted-foreground">{t("MultipleDatabases.description")}</p>
+      </div>
+      <Card>
+        <CardContent className="space-y-4">
+          <div className="grid gap-4 pt-4 box-content">
+            {data.databases.map((database) => (
+              <div
+                key={database.database_id}
+                className={cn(
+                  "w-full p-2 flex items-center justify-between rounded-md cursor-pointer overflow-hidden transition-colors",
+                  database.database_id === selectedDatabase
+                    ? "bg-primary text-primary-foreground"
+                    : "hover:bg-accent hover:text-accent-foreground"
+                )}
+                onClick={() => setSelectedDatabase(database.database_id)}
+              >
+                <div className="flex-1 overflow-hidden">
+                  <h3 className="font-medium">{database.database_title}</h3>
+                  <Link
+                    href={database.database_url}
+                    target="_blank"
+                    className="text-sm text-muted-foreground hover:underline"
+                    prefetch={false}
+                  >
+                    <span className="truncate block">
+                      {database.database_url}
+                    </span>
+                  </Link>
+                </div>
 
-              {database.database_id === selectedDatabase && (
-                <CheckCheckIcon size={20} className="w-6 h-6" />
-              )}
-            </div>
-          ))}
-        </div>
-      </CardContent>
-      <CardFooter className="flex justify-end gap-2">
-        {/* <Button variant="outline">取消</Button> */}
-        <Button onClick={saveReq.run} className="space-x-1">
-          {saveReq.loading && (
-            <LoaderCircle size={16} className="w-4 h-4 animate-spin" />
-          )}
-          <span>Save</span>
-        </Button>
-      </CardFooter>
-    </Card>
+                {database.database_id === selectedDatabase && (
+                  <CheckCheckIcon size={20} className="w-6 h-6" />
+                )}
+              </div>
+            ))}
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-end gap-2">
+          {/* <Button variant="outline">取消</Button> */}
+          <Button onClick={saveReq.run} className="space-x-1">
+            {saveReq.loading && (
+              <LoaderCircle size={16} className="w-4 h-4 animate-spin" />
+            )}
+            <span>Save</span>
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
   );
 };
 
--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,31 @@
 import createMiddleware from "next-intl/middleware";
-import { defaultLocale, localePrefix, locales } from "./lib/navigation";
-
-export default createMiddleware({
-  locales,
-  localePrefix,
+import {
   defaultLocale,
-  localeDetection: false,
-});
+  localeItems,
+  localePrefix,
+  locales,
+} from "./lib/navigation";
+import { NextRequest } from "next/server";
+
+export default (request: NextRequest) => {
+  const acceptLanguage = request.headers.get("accept-language");
+  const default_locale =
+    localeItems.find((v) => acceptLanguage?.includes(v.iso))?.code ||
+    defaultLocale;
+
+  return createMiddleware({
+    locales,
+    localePrefix,
+    defaultLocale: default_locale,
+    localeDetection: false,
+  })(request);
+};
 
 export const config = {
   // Include all paths that should be internationalized,
   // including the "api" folder, and excluding "_next" folder
   // and all files with an extension (e.g. favicon.ico)
-  matcher: ["/((?!_next|.*\\..*).*)"],
+  matcher: ["/((?!_next|.*\\..*|api/.*).*)"],
 };
 
 export const runtime = "experimental-edge";
