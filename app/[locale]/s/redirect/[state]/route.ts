74a547c2ce47206c1220713f6727dd14dcef3fdd
SengMitnick <sengmitnick@163.com>
2024-07-11 12:36:04 +0800

    feat: Add import statement for constant variables
    The code changes add an import statement for the `clientId` and `redirectUri` variables from the `@/lib/constant` module in the `route.ts` file. This allows the code to use these variables instead of directly accessing the environment variables.

--- app/[locale]/s/redirect/[state]/route.ts
+++ app/[locale]/s/redirect/[state]/route.ts
@@ -1,3 +1,4 @@
+import { clientId, redirectUri } from "@/lib/constant";
 import { NextRequest, NextResponse } from "next/server";
 
 export const runtime = "edge";
@@ -9,13 +10,13 @@ export function GET(
   if (!params.state) {
     return NextResponse.json({ error: "Missing state" }, { status: 400 });
   }
-  const redirectUri = process.env.NOTION_OAUTH_REDIRECT_URI!;
+
   const url = new URL("https://api.notion.com/v1/oauth/authorize");
 
   url.searchParams.append("owner", "user");
   url.searchParams.append("response_type", "code");
   url.searchParams.append("redirect_uri", redirectUri);
-  url.searchParams.append("client_id", process.env.NOTION_OAUTH_CLIENT_ID!);
+  url.searchParams.append("client_id", clientId);
   url.searchParams.append("state", params.state);
   return NextResponse.redirect(url.toString());
 }
--- lib/constant.ts
+++ lib/constant.ts
@@ -1,2 +1,9 @@
 export const SELECTD_FORMS = "FormList";
 export const SELECTED_FORM = "SelectedForm";
+
+export const clientId = process.env.NOTION_OAUTH_CLIENT_ID!;
+export const clientSecret = process.env.NOTION_OAUTH_CLIENT_SECRET!;
+export const redirectUri =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/auth/notion/callback"
+    : process.env.NOTION_OAUTH_REDIRECT_URI!;
