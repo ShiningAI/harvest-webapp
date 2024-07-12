11662f9df1e67b30ac40d72343b122845d3b1df2
SengMitnick <sengmitnick@163.com>
2024-07-12 09:58:45 +0800

    feat: Update layout component to adjust height dynamically
    The layout component in the `[locale]/s` directory has been updated to dynamically adjust its height based on the content. This change improves the user experience by ensuring that the layout always fits the content.

--- app/[locale]/s/layout.tsx
+++ app/[locale]/s/layout.tsx
@@ -1,3 +1,10 @@
 import { RootLayout } from "@/components/RootLayout";
+import { PropsWithChildren } from "react";
 
-export default RootLayout;
+export default function Layout({ children }: PropsWithChildren) {
+  return (
+    <RootLayout className="w-full flex flex-col items-center">
+      {children}
+    </RootLayout>
+  );
+}
