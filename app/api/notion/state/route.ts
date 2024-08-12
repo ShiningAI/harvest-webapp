ff66703ae96b8d629404897e7f8368ddf14cb77a
SengMitnick <sengmitnick@163.com>
2024-08-12 22:09:58 +0800

    feat: Add route for creating state in Notion API

---
+++ app/api/notion/state/route.ts
@@ -0,0 +1,16 @@
+
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+
+export async function POST() {
+    const session = await auth();
+
+    if (!session?.user) {
+        return NextResponse.json({ ok: false });
+    }
+
+    const data = { t: Date.now(), wxId: session.user.id };
+    const state = Buffer.from(JSON.stringify(data)).toString("base64");
+
+    return NextResponse.json({ ok: true, data: state });
+}
