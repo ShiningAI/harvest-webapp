871f5b9e9f9359f4fd7c2b856bc5ac6f9c5acf23
SengMitnick <sengmitnick@163.com>
2024-08-12 22:27:54 +0800

    feat: Update login page with router navigation and redirect on login

--- app/[locale]/(webapp)/login/page.tsx
+++ app/[locale]/(webapp)/login/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { SignInContent } from "@/components/SignIn";
 
 export default function Page() {
+  const router = useRouter();
   return (
     <div className="flex flex-1 h-full p-0 w-auto flex-col overflow-hidden md:flex-row">
-      <SignInContent />
+      <SignInContent onLogin={() => router.replace("/user/accounts")} />
     </div>
   );
 }
