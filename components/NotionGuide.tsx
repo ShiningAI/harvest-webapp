5f4405bf6fd694e4f265d3cef2dd6ea9606ad4ad
ray <haozanlawyer@gmail.com>
2024-08-20 16:23:13 +0800

    feat: Add NotionGuide component for user select page

--- components/NotionGuide.tsx
+++ components/NotionGuide.tsx
@@ -5,14 +5,15 @@ import { Button } from "@/components/ui/button";
 
 interface Props {
     state: string;
+    rebind?: boolean;
 }
 
-export const NotionGuide = ({ state }: Props) => {
+export const NotionGuide = ({ state, rebind }: Props) => {
     const t = useTranslations("Database");
     return (
         <div className="mx-auto w-full max-w-md space-y-6 py-6">
             <Link href={`/s/${state}`}>
-                <Button>{t("Authorize.goto")}</Button>
+                <Button>{rebind ? t("Authorize.rebind") : t("Authorize.goto")}</Button>
             </Link>
         </div>
     );
