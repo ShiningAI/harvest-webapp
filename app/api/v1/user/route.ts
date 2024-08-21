eb7cc1400ce35ff4191272034630a4e2597ec21e
SengMitnick <sengmitnick@163.com>
2024-08-21 10:36:52 +0800

    feat: Add GET route for user data retrieval
    This commit adds a new `GET` route in the `user/route.ts` file to retrieve user data. It uses the `auth` function from the `@/auth` module to authenticate the user. If the user is not authenticated, it returns a JSON response with a status of 401. Otherwise, it fetches user data from the `http://api.notion-nice.com/mp/user` endpoint and returns it as a JSON response. The response includes the user's ID, name, and a state parameter generated from the data.

---
+++ app/api/v1/user/route.ts
@@ -0,0 +1,38 @@
+
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    const session = await auth();
+
+    if (!session?.user) {
+        return NextResponse.json({ ok: false }, { status: 401 });
+    }
+
+    const data = { t: Date.now(), unionid: session.user.id };
+    const state = Buffer.from(JSON.stringify(data)).toString("base64");
+
+    const response = await fetch(`http://api.notion-nice.com/mp/user?unionid=${data.unionid}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+
+    if (!response.ok) {
+        return NextResponse.json({ ok: false, error: "Notion auth failed" });
+    }
+
+    const respJson = await response.json<any>();
+
+    const user = {
+        ...respJson.data,
+        id: session.user.id,
+        name: respJson.data.name || session.user.name,
+        state
+    };
+
+    return NextResponse.json({ ok: true, data: user });
+}
+
+export const runtime = 'edge';
