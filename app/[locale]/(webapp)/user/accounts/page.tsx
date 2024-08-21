5c84e7e9a6a314bbcd4109cc8de7182a35d70ccc
SengMitnick <sengmitnick@163.com>
2024-08-21 10:56:40 +0800

    feat: Add lazy loading for user avatar and WeChat database selection

--- app/[locale]/(webapp)/user/accounts/page.tsx
+++ app/[locale]/(webapp)/user/accounts/page.tsx
@@ -50,7 +50,29 @@ export default function Page() {
       ></Item>
       <Item
         title="Notion"
-        desc={user.access_token ? t("bound") : t("desc")}
+        desc={
+          user.access_token ? (
+            <>
+              <div>{user.email || t("bound")}</div>
+              {!!user.database?.database_id && (
+                <span>
+                  {t("selected")}
+                  <Link
+                    className="ml-1 text-primary underline"
+                    href={user.database.database_url}
+                  >
+                    {user.database.database_title}
+                  </Link>
+                  <Link className="ml-1 text-blue-500" href="/databases/select">
+                    {t("change")}
+                  </Link>
+                </span>
+              )}
+            </>
+          ) : (
+            t("desc")
+          )
+        }
         icon={<IconBrandNotion size={24} />}
       >
         {user.state &&
@@ -71,24 +93,26 @@ export default function Page() {
 type ItemProps = PropsWithChildren<{
   icon: React.ReactNode;
   title: string;
-  desc: string;
+  desc: React.ReactNode;
 }>;
 
 function Item({ icon, title, desc, children }: ItemProps) {
   return (
-    <div className="mb-2 flex items-center rounded-lg border-[0.5px] border-gray-200 bg-gray-50 px-3 py-2">
-      <div className="mr-3 h-8 w-8 rounded-lg border border-gray-100 bg-white flex items-center justify-center">
-        {icon}
-      </div>
-      <div className="flex-1">
-        <div className="text-sm font-medium leading-[21px] text-gray-800">
-          {title}
+    <div className="mb-2 flex flex-col md:flex-row md:items-center gap-2 rounded-lg border-[0.5px] border-gray-200 bg-gray-50 px-3 py-2">
+      <div className="flex-1 flex items-center">
+        <div className="mr-3 h-8 w-8 rounded-lg border border-gray-100 bg-white flex items-center justify-center">
+          {icon}
         </div>
-        <div className="text-xs font-normal leading-[18px] text-gray-500">
-          {desc}
+        <div className="flex-1">
+          <div className="text-sm font-medium leading-[21px] text-gray-800">
+            {title}
+          </div>
+          <div className="flex flex-col text-xs font-normal leading-[18px] text-gray-500">
+            {desc}
+          </div>
         </div>
       </div>
-      {children}
+      <div className="flex justify-end">{children}</div>
     </div>
   );
 }
