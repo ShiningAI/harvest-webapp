9679ea00510f8727436ef444ac9de69314b1c967
SengMitnick <sengmitnick@163.com>
2024-09-12 15:47:08 +0800

    feat: Add lazy loading for images in pricing page
    The code changes in `page.tsx` implement lazy loading for images in the pricing page. This change improves the page load performance by loading images only when they are visible to the user, reducing the initial load time and improving the overall user experience.

--- app/[locale]/(webapp)/pricing/page.tsx
+++ app/[locale]/(webapp)/pricing/page.tsx
@@ -1,8 +1,32 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRequest } from "ahooks";
+
 function Page() {
-    return <div className="mt-20">Todo...</div>;
-  }
-  
-  export default Page;
-  
-  export const runtime = "edge";
-  
+  const { run } = useRequest(
+    async () => {
+      const resp = await fetch("/api/v1/pay", {
+        method: "POST",
+        body: JSON.stringify({ type: "native" }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const resp_json: any = await resp.json();
+      console.log(resp_json);
+    },
+    {
+      manual: true,
+    }
+  );
+  return (
+    <div className="mt-20">
+      <Button onClick={run}>Click me</Button>
+    </div>
+  );
+}
+
+export default Page;
+
+export const runtime = "edge";
