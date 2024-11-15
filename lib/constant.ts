3b26bc6e83987a213f4d2b8a75d7e7becbfb9aa1
SengMitnick <sengmitnick@163.com>
2024-11-15 22:02:31 +0800

    feat: 更新重定向和 API URL 以使用新的生产环境地址

--- lib/constant.ts
+++ lib/constant.ts
@@ -6,16 +6,18 @@ export const clientSecret = process.env.NOTION_OAUTH_CLIENT_SECRET!;
 export const redirectUri =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/api/auth/callback/notion"
-    : "https://harvest.superai42.com/api/auth/callback/notion";
+    : "https://harvest.prius.ai/api/auth/callback/notion";
 
 export const redirectUrl =
   process.env.NODE_ENV === "test"
     ? "http://localhost:3000/api/notion"
-    : "https://harvest.superai42.com/api/notion";
+    : "https://harvest.prius.ai/api/notion";
 
-export const API_NICE_URL = process.env.NODE_ENV === "test"
-  ? "http://localhost:8000"
-  : "http://api.notion-nice.com";
-export const NOTION_OAUTH_HOST = process.env.NODE_ENV === "test" ?
-  "http://localhost:3000" :
-  'https://harvest.superai42.com';
+export const API_NICE_URL =
+  process.env.NODE_ENV === "test"
+    ? "http://localhost:8000"
+    : "http://api.notion-nice.com";
+export const NOTION_OAUTH_HOST =
+  process.env.NODE_ENV === "test"
+    ? "http://localhost:3000"
+    : "https://harvest.prius.ai";
