a175151da837d3abe1f1bd9e768776f88c9680fb
SengMitnick <sengmitnick@163.com>
2024-11-18 00:26:18 +0800

    feat: 更新支付成功页面，替换图标并调整按钮布局以优化用户体验

--- app/[locale]/(webapp)/payment/success/page.tsx
+++ app/[locale]/(webapp)/payment/success/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { CircleCheckIcon } from "lucide-react";
+import { CheckCircleIcon, CircleCheckIcon } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
 import { QRCode } from "./QRCode";
@@ -12,21 +12,23 @@ export const metadata: Metadata = {
 export const runtime = "edge";
 const PaymentSuccess = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-6 py-12 md:py-36">
-      <CircleCheckIcon className="size-12 text-green-500" />
+    <div className="flex flex-col items-center justify-center gap-6 py-8 md:py-36">
+      <CheckCircleIcon className="size-9 text-green-500" />
       <div className="space-y-2 text-center">
         <h2 className="text-2xl font-bold">支付成功</h2>
         <p className="text-muted-foreground">
           您的支付已经成功完成。感谢您的支持!
         </p>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-        <Link href="/">
-          <Button variant="secondary">返回首页</Button>
-        </Link>
-        <Link href="/user/accounts">
-          <Button variant="outline">绑定账号</Button>
-        </Link>
+      <div className="flex flex-col items-center md:flex-row gap-4">
+        <div className="flex gap-4">
+          <Link href="/">
+            <Button variant="secondary">返回首页</Button>
+          </Link>
+          <Link href="/user/accounts">
+            <Button variant="outline">绑定账号</Button>
+          </Link>
+        </div>
         <Link
           href="https://mp.weixin.qq.com/s/TyRMsprIZskL46zYnK7rEA"
           target="_blank"
