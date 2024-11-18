810e845b0897c76e3c51ce81a83c0cda50e74bfa
SengMitnick <sengmitnick@163.com>
2024-11-18 22:58:39 +0800

    feat: 优化 Notion API 路由，增加错误处理并重构代码以提高可读性

--- app/api/notion/route.ts
+++ app/api/notion/route.ts
@@ -1,4 +1,10 @@
-import { redirectUri, clientId, clientSecret, API_NICE_URL, redirectUrl } from "@/lib/constant";
+import {
+  redirectUri,
+  clientId,
+  clientSecret,
+  API_NICE_URL,
+  redirectUrl,
+} from "@/lib/constant";
 import { NextRequest, NextResponse } from "next/server";
 
 export const runtime = "edge";
@@ -7,54 +13,63 @@ export const runtime = "edge";
 const encoded = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
 
 export async function GET(req: NextRequest) {
-    const searchParams = req.nextUrl.searchParams;
-    const code = searchParams.get("code")
-    const state = searchParams.get("state")
+  const searchParams = req.nextUrl.searchParams;
+  const code = searchParams.get("code");
+  const state = searchParams.get("state");
+  const errorMsg = searchParams.get("error");
+  const url = req.nextUrl.clone();
 
-    if (!code || !state || !clientId || !clientSecret || !redirectUri) {
-        return NextResponse.json({ ok: false, error: "Invalid code" }, { status: 401 });
+  if (errorMsg) {
+    url.pathname = "/user/accounts";
+    return NextResponse.redirect(url);
+  }
+
+  if (!code || !state || !clientId || !clientSecret || !redirectUri) {
+    return NextResponse.json(
+      { ok: false, error: "Invalid code" },
+      { status: 401 }
+    );
+  }
+
+  url.pathname = "/databases/select";
+  try {
+    const s = decodeURIComponent(state);
+    const decode = JSON.parse(Buffer.from(s, "base64").toString("utf8"));
+
+    const data: any = {
+      code,
+      redirect_uri: redirectUrl,
+      grant_type: "authorization_code",
+    };
+
+    if (decode.unionid) {
+      data.unionid = decode.unionid;
     }
 
-    const url = req.nextUrl.clone();
-    url.pathname = "/databases/select";
-    try {
-        const s = decodeURIComponent(state);
-        const decode = JSON.parse(Buffer.from(s, "base64").toString("utf8"));
+    console.log(
+      `[${req.method}]/v1/oauth/token`,
+      JSON.stringify(data, null, 2)
+    );
 
-        const data: any = {
-            code,
-            'redirect_uri': redirectUrl,
-            grant_type: 'authorization_code'
-        }
+    const notion_auth_resp = await fetch(`${API_NICE_URL}/v1/oauth/token`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        Authorization: `Basic ${encoded}`,
+        "Content-Type": "application/json",
+      },
+    });
 
-        if (decode.unionid) {
-            data.unionid = decode.unionid;
-        }
+    const notion_auth_resp_json: any = await notion_auth_resp.json();
 
-        console.log(`[${req.method}]/v1/oauth/token`, JSON.stringify(data, null, 2));
-
-        const notion_auth_resp = await fetch(
-            `${API_NICE_URL}/v1/oauth/token`,
-            {
-                method: "POST",
-                body: JSON.stringify(data),
-                headers: {
-                    Authorization: `Basic ${encoded}`,
-                    "Content-Type": "application/json"
-                },
-            }
-        );
-
-        const notion_auth_resp_json: any = await notion_auth_resp.json();
-
-        if (!notion_auth_resp_json.access_token) {
-            url.searchParams.append("error", "access_token not found")
-            return NextResponse.redirect(url)
-        }
-
-        return NextResponse.redirect(url)
-    } catch (error: any) {
-        url.searchParams.append("error", error.message || "Notion auth failed")
-        return NextResponse.redirect(url)
+    if (!notion_auth_resp_json.access_token) {
+      url.searchParams.append("error", "access_token not found");
+      return NextResponse.redirect(url);
     }
+
+    return NextResponse.redirect(url);
+  } catch (error: any) {
+    url.searchParams.append("error", error.message || "Notion auth failed");
+    return NextResponse.redirect(url);
+  }
 }
